9319c3dcaebbc56bf2a6486773459282d8ea6295
Joel Palmius <joepal1976@hotmail.com>
2016-12-07 20:12:08 +0100

    Bana med andreas gubbe och lite händelser
    Här har marken, bollen och väggarna fått "fysiska" material som reglerar
    hur hårda studsar blir. Vidare finns det en "fälla" som skickar upp
    spelarmeshen en bit i luften om man kliver på den.

---
+++ Assets/Scripts/Uptrap.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Uptrap : MonoBehaviour
+{
+    public float hoverForce;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger", other);
+        //Destroy(other.gameObject);
+        //GetComponent<AudioSource>().Play();
+
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.up * 700, ForceMode.Acceleration);
+        //rb.AddForce(Vector3.back * -500, ForceMode.Acceleration);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collission");
+    }
+}
